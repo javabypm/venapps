dc9b23f8b58850b69025db40cb2d2e61c3ae679a
pm <pm@ppm>
2017-08-30 22:25:43 +0530

    2 nd Change

--- src/com/venapp/login/test.java
+++ src/com/venapp/login/test.java
@@ -3,4 +3,24 @@ package com.venapp.login;
 public class test {
 	int eid;
 	String ename;
+	Double esal;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Double getEsal() {
+		return esal;
+	}
+	public void setEsal(Double esal) {
+		this.esal = esal;
+	}
+	
 }
